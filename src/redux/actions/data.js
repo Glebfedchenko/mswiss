05583c5d917666e6b6131b5df49806486c229e60
Gleb Fedchenko <fedchenkogleb@gmail.com>
2018-10-19 23:48:52 +0300

    added actions and reducers for redux part of project

--- src/redux/actions/data.js
+++ src/redux/actions/data.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = `https://www.mocky.io/v2/5bc71805320000642c0b0646`; // submitted json to mock server
 export const GET_DATA = `GET_DATA`;
+export const SEARCH = `SEARCH`;
+export const SEARCH_BY_TAG = `SEARCH_BY_TAG`;
 
 export const getData = () => {
   const request = axios.get(BASE_URL).then(res => res.data);
@@ -10,3 +12,15 @@ export const getData = () => {
     payload: request,
   };
 };
+
+// search action in case project is run with redux
+export const searchData = value => ({
+  type: SEARCH,
+  payload: value,
+});
+
+// search by tag action in case project is run with redux
+export const searchByTag = value => ({
+  type: SEARCH_BY_TAG,
+  payload: value,
+});
--- src/redux/reducers/data.js
+++ src/redux/reducers/data.js
@@ -8,6 +8,7 @@ export const data = (
   state = {
     searchValue: '',
     results: sdks.results,
+    displayResults: [], // in case project is run with redux
     uniqueTags,
   },
   action,
@@ -17,6 +18,23 @@ export const data = (
       return {
         ...state,
         results: action.payload,
+        displayResults: action.payload.results, // in case project is run with redux
+      };
+    case actions.SEARCH: // action for search in case project is run with redux
+      return {
+        ...state,
+        searchValue: action.payload,
+        results: state.displayResults.filter(r =>
+          r.title.toLowerCase().includes(action.payload.toLowerCase()),
+        ),
+      };
+    case action.SEARCH_BY_TAG: // action for search by tag in case project is run with redux
+      return {
+        ...state,
+        searchValue: action.payload,
+        displayResults: state.displayResults.filter(r =>
+          r.tags.toLowerCase().includes(action.payload.toLowerCase()),
+        ),
       };
     default:
       return state;
